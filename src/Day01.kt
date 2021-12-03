07c9784ba9fe56370a8f71e24c08a607c304b4d0
irgregg <irgregg@comcast.net>
2021-12-02 20:56:40 -0800

    Day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
+import java.util.stream.Collectors
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val asInts = input.stream().map {it.toInt()}
+        var soln = 0
+        val itr = asInts.iterator()
+        var previous = Int.MAX_VALUE
+        while(itr.hasNext()) {
+            val cur = itr.next()
+            if (cur > previous) soln++
+            previous = cur
+        }
+        return soln
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val asInts = input.stream().map { it.toInt() }.collect(Collectors.toList())
+        var soln = 0
+        for (i in 0..asInts.size - 4) {
+            if (asInts[i] < asInts[i + 3]) {
+                soln++
+            }
+        }
+        return soln
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("day1_1_test")
+    println(part2(testInput))
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("day1_1")
     println(part1(input))
     println(part2(input))
 }
